03b06992e8683ca0bb86b466bb8dec6bcd470251
Chunlin.Zhao <zhaochunlin_job@163.com>
2015-01-10 21:44:09 +0800

    init maven webapp demo

---
+++ maven-demo-web/src/main/java/demo/maven/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package demo.maven;
+
+/**
+ * 这是一个测试出
+ * @author zhaochunlin
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
