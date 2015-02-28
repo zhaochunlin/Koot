b7cd28981511ebe337e49a219d301bc84b8cf330
chunlin.zhao <zhaochunlin_job@163.com>
2015-02-28 14:39:27 +0800

    添加Spring MVC相关配置

---
+++ maven-demo-web/src/main/java/demo/maven/controller/MainController.java
@@ -0,0 +1,41 @@
+
+
+package demo.maven.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * ClassName:MainController <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason: TODO ADD REASON. <br/>
+ * Date: 2015年2月28日 下午2:29:34 <br/>
+ * @author chunlin.zhao
+ * @version
+ * @since 1.0
+ * @see
+ */
+
+@Controller
+@RequestMapping("/mvc/")
+public class MainController {
+
+    /**
+     * 首页
+    * main:(这里用一句话描述这个方法的作用). <br/>
+    * TODO(这里描述这个方法适用条件 – 可选).<br/>
+    * TODO(这里描述这个方法的执行流程 – 可选).<br/>
+    * TODO(这里描述这个方法的使用方法 – 可选).<br/>
+    * TODO(这里描述这个方法的注意事项 – 可选).<br/>
+    *
+    * @author chunlin.zhao
+    * @return
+    * @since 1.0
+     */
+    
+    @RequestMapping("/index")
+    public String main(){
+        return "welcome";
+    }
+}
+
